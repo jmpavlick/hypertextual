0012e92aada886cadcd41c4e970e5839432e9ed6
John Pavlick <jmpavlick@gmail.com>
2016-01-02 07:33:34 -0500

    fixing fucking everything

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,12 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+# for heroku without a database adapter
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'active_model/railtie'
+require 'sprockets/railtie'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -21,6 +27,7 @@ module Hypertextual
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    # LOL WE HAVE NO DATABASE GET OUT OF HERE
+    # config.active_record.raise_in_transactional_callbacks = true
   end
 end
