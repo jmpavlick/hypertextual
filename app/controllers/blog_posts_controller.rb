31d90281a37009e6967c48a1ff91a52eea4349a6
John Pavlick <jmpavlick@gmail.com>
2016-01-02 15:32:14 -0500

    heroku dir sort fix

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,6 +1,6 @@
 class BlogPostsController < ApplicationController
   def index
-    posts = Dir[Rails.root + "public/posts/*.md"]
+    posts = Dir[Rails.root + "public/posts/*.md"].sort
     @postindex = params[:p] == nil ? posts.count - 1 : params[:p].to_i
     renderer = Redcarpet::Render::HTML.new(render_options = {})
     markdown = Redcarpet::Markdown.new(renderer, extensions = {})
@@ -14,10 +14,6 @@ class BlogPostsController < ApplicationController
   end
 
   def allposts
-    # post title is derived from filename by removing the date prefix
-    # then adding a single space character at every new capital letter
-    # i.e., 20150101_HelloHowAreYou becomes Hello How Are You
-    # @posts = Dir[Rails.root + "public/posts/*.md"].map { |x| File.basename(x, ".*").split("_")[1].split(/(?=[A-Z])/).join(" ") }
-    @posts = Dir[Rails.root + "public/posts/*.md"].map { |x| titleify(File.basename(x, ".*")) }
+    @posts = Dir[Rails.root + "public/posts/*.md"].sort.map { |x| titleify(File.basename(x, ".*")) }
   end
 end
